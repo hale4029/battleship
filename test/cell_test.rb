51b68af48ccd513592da988e1c76a452885cf8f8
Dylan Connolly <dylanconnolly145@gmail.com>
2019-10-08 11:34:45 -0600

    Add cell test for empty cell and ship placement

--- test/cell_test.rb
+++ test/cell_test.rb
@@ -11,8 +11,19 @@ class CellTest < Minitest::Test
     assert_instance_of Cell, cell
   end
 
-  def def_it_has_a_coordinate
+  def test_it_has_a_coordinate
     assert_equal "B4", cell.coordinate
   end
 
-  def
+  def test_cell_is_empty
+    assert_equal nil, cell.ship
+    assert_equal true, cell.empty?
+  end
+
+  def test_a_ship_can_be_placed_on_cell
+    cruiser = Ship.new("Cruiser", 3)
+    cell.place_ship(cruiser)
+
+    assert_equal cruiser, cell.ship
+    assert_equal false, cell.empty?
+  end
