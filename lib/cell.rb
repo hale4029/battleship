cd7b751d04adf7d40e9df27e7d7d28e254f96915
Harrison Levin <harrison483@gmail.com>
2019-10-08 14:47:18 -0600

    outline of test and lib for cell class

---
+++ lib/cell.rb
@@ -0,0 +1,25 @@
+
+
+class Cell
+  attr_accessor :coordinate
+
+  def initialize(coordinate)
+    @coordinate = coordinate
+  end
+
+  def ship
+  end
+
+  def place_ship
+  end
+
+  def empty?
+  end
+
+  def fired_upon?
+  end
+
+  def render
+  end
+
+end
---
+++ test/cell_test.rb
@@ -0,0 +1,28 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/ship'
+require "./lib/cell"
+
+class CellTest < Minitest::Test
+  def setup
+  end
+
+  def test_if_the_cell_contains_a_ship
+  end
+
+  def test_ship_placement
+  end
+
+  def test_if_cell_is_empty
+  end
+
+  def test_if_cell_is_fired_upon
+  end
+
+  def test_if_cell_registers_a_hit
+  end
+
+  def test_render
+  end 
+
+end
