939c8e77d5d7a39c12f12655b6bf1c9912b1b9ab
Dylan Connolly <dylanconnolly145@gmail.com>
2019-10-08 14:35:07 -0600

    Add cell rendering tests and change cell to instance variable

--- test/cell_test.rb
+++ test/cell_test.rb
@@ -4,26 +4,73 @@ require 'minitest/pride'
 class CellTest < Minitest::Test
 
   def setup
-    cell = Cell.new("B4")
+    @cell = Cell.new("B4")
   end
 
   def test_it_exists
-    assert_instance_of Cell, cell
+    assert_instance_of Cell, @cell
   end
 
   def test_it_has_a_coordinate
-    assert_equal "B4", cell.coordinate
+    assert_equal "B4", @cell.coordinate
   end
 
   def test_cell_is_empty
-    assert_equal nil, cell.ship
-    assert_equal true, cell.empty?
+    assert_equal nil, @cell.ship
+    assert_equal true, @cell.empty?
   end
 
   def test_a_ship_can_be_placed_on_cell
     cruiser = Ship.new("Cruiser", 3)
-    cell.place_ship(cruiser)
+    @cell.place_ship(cruiser)
 
-    assert_equal cruiser, cell.ship
-    assert_equal false, cell.empty?
+    assert_equal cruiser, @cell.ship
+    assert_equal false, @cell.empty?
   end
+
+  def test_cell_can_be_fired_upon
+    cruiser = Ship.new("Cruiser", 3)
+    @cell.place_ship(cruiser)
+
+    assert_equal false, @cell.fired_upon?
+
+    @cell.fire_upon
+
+    assert_equal 2, @cell.ship.health
+    assert_equal true, @cell.fired_opon?
+  end
+
+  def test_render_is_dynamic_for_miss
+
+    assert_equal ".", @cell.render
+
+    @cell.fire_upon
+
+    assert_equal "M", @cell.render
+  end
+
+  def test_render_is_dynamic_for_ship_placement_and_hit
+    cell_2 = Cell.new("C3")
+    cruiser = Ship.new("Cruiser", 3)
+
+    assert_equal ".", cell_2.render
+
+    cell_2.place_ship(cruiser)
+
+    assert_equal "S", cell_2.render(true)
+
+    cell_2.fire_upon
+
+    assert_equal "H", cell_2.render
+  end
+
+  def test_render_is_dynamic_for_sunken_ship
+    cell_2 = Cell.new("C3")
+    cruiser = Ship.new("Cruiser", 3)
+    cell_2.place_ship(cruiser)
+    cell_2.fire_upon
+    cell_2.
+
+    assert_equal
+  end
+end
