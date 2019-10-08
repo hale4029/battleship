e694f28ea75713e9217a03272e0861e1d94815b5
Dylan Connolly <dylanconnolly145@gmail.com>
2019-10-08 16:49:12 -0600

    Add render method to cell class with optional boolean

--- lib/cell.rb
+++ lib/cell.rb
@@ -1,9 +1,52 @@
 class Cell
   attr_reader :coordinate
 
+  attr_accessor :ship,
+                :fired_upon
+
   def initialize(coordinate)
     @coordinate = coordinate
+    @ship = nil
+    # @empty = true # may not need this (if ship = nil, empty is implied, if ship = nil && fired_upon = true, render should be "M")
+    @fired_upon = false
   end
 
-  def
+  def empty?
+    if @ship == nil
+      true
+    else
+      false
+    end
+  end
+
+  def place_ship(ship_name)
+    @ship = ship_name
+  end
+
+  def fired_upon?
+    @fired_upon
+  end
+
+  def fire_upon
+    if @ship != nil
+    @ship.health -= 1
+    end
+    @fired_upon = true
+  end
+
+  def render(player = false)
+    if player == true && @ship != nil && @fired_upon == false
+        "S"
+    elsif @ship == nil && @fired_upon == false
+        "."
+    elsif @ship == nil && @fired_upon == true
+        "M"
+    elsif @ship != nil && @fired_upon == true && @ship.health <= 0
+        "X"
+    elsif @ship != nil && @fired_upon == true
+        "H"
+    else
+      "."
+    end
+  end
 end
