27fe4b91e29fc29a6675b2c6d847c1446589d478
Dylan Connolly <dylanconnolly145@gmail.com>
2019-10-09 16:00:13 -0600

    Add additional assertions to ship placement test

--- test/board_test.rb
+++ test/board_test.rb
@@ -28,13 +28,27 @@ class BoardTest < Minitest::Test
     assert_equal false, @board.valid_coordinate?(1)
   end
 
+  def test_if_row_placement_is_valid
+    cruiser = Ship.new("Cruiser", 3)
+    submarine = Ship.new("Submarine", 2)
+
+    assert_equal true, @board.valid_rows(cruiser, ["A3", "A4"])
+    assert_equal true, @board.valid_rows(submarine, ["A2", "B3", "C4"])
+    assert_equal false, @board.valid_rows(submarine, ["A2", "B3", "A4"])
+    assert_equal false, @board.valid_rows(submarine, ["A2", "B3", "D4"])
+  end
+
   def test_if_ship_placement_is_valid
     cruiser = Ship.new("Cruiser", 3)
     submarine = Ship.new("Submarine", 2)
 
     assert_equal false, @board.valid_placement?(cruiser, ["A3", "A4"])
     assert_equal false, @board.valid_placement?(submarine, ["A2", "A3", "A4"])
+    # require "pry"; binding.pry
     assert_equal true, @board.valid_placement?(cruiser, ["A1", "A2","A3"])
     assert_equal true, @board.valid_placement?(submarine, ["A2", "A3"])
+    assert_equal true, @board.valid_placement?(cruiser, ["A1", "B1", "C1"])
+    assert_equal false, @board.valid_placement?(cruiser, ["A1", "B2", "C1"])
+    assert_equal false, @board.valid_placement?(cruiser, ["A1", "B2", "C3"])
   end
 end
