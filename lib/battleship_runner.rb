30a3986e2d2c34675a6d30784d59431aed92872e
Dylan Connolly <dylanconnolly145@gmail.com>
2019-10-15 11:25:13 -0600

    Add place_cruiser game method and create runner file

---
+++ lib/battleship_runner.rb
@@ -0,0 +1,9 @@
+require './lib/game'
+require './lib/board'
+require './lib/cell'
+require './lib/ship'
+
+
+game = Game.new
+
+game.start_game
--- lib/game.rb
+++ lib/game.rb
@@ -6,26 +6,38 @@ class Game
   attr_reader :board,
               :input
 
-  @board = Board.new
-
   def main_menu
     puts "Welcome to BATTLESHIP"
-    puts "To start a new game, enter 'p'. To quit game, hit 'q'."
+    puts "To start a new game, enter 'p'. To quit game, enter 'q'."
     print ">"
     @input = gets.chomp.downcase
+  end
+
+  def place_cruiser
+    puts "Enter the spaces for the cruiser (ex: A1 A2 A3)"
+    print ">"
+    cruiser_placement = gets.chomp.split(" ")
+    @player_board.
   end
 
   def start_game
       main_menu
       if @input = "p"
+
+        @computer = ComputerBoard.new
+
+        puts "Computer's Board"
+        print @computer.computer_board.render
+        puts "=========="
+        puts "Your Board"
+        @player_board = Board.new
+        print @player_board.render(true)
         puts "You will need to lay out your ships on the board"
-        puts "The cruiser is 3 units long and the submarine is 2 units long"
-        @board.render(true)
-        puts "Enter the spaces for the cruiser (ex: [A1, A2, A3]"
-        print ">"
-        array = gets.chomp.to_a
+        puts "The cruiser is 3 units long and the submarine is 2 units long."
+        place_cruiser
       else
         puts "Goodbye!"
+      end
   end
 
   def take_turn
