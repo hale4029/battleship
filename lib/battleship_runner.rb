8138864732fc6a3e36d337fe36801a21b7384265
Dylan Connolly <dylanconnolly145@gmail.com>
2019-10-15 14:03:23 -0600

    Add until loop for take_turn

--- lib/battleship_runner.rb
+++ lib/battleship_runner.rb
@@ -7,5 +7,7 @@ require './lib/ship'
 game = Game.new
 
 game.start_game
-until game.
+until (game.player_cruiser.health + game.player_sub.health == 0) || (game.computer.cruiser.health + game.computer.submarine.health == 0)
 game.take_turn
+end
+puts "Game Over."
