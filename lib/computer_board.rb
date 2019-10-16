cf3170939bc6262dccdeab009e10b94cdc641359
Harrison Levin <harrison483@gmail.com>
2019-10-16 12:12:51 -0600

    complete refactor of computer board

--- lib/computer_board.rb
+++ lib/computer_board.rb
@@ -10,40 +10,51 @@ class ComputerBoard
     @computer_board = Board.new(@dimensions)
     @computer_board.create_cells
     @ships = [@cruiser = Ship.new("Cruiser", 3), @submarine = Ship.new("Submarine", 2)]
+    @ship = nil
+  end
+
+  def horizontal_placement
+    x = 0
+    until x == 1
+      letters = (65..(65 + (@dimensions - 1))).to_a.map { |ord| ord.chr}
+      numbers = (1..(1 + (@dimensions - 1))).to_a
+      letters = letters.each_cons(@ship.length).to_a.sample
+      numbers = ([numbers.each_cons(@ship.length).to_a.sample.sample] * (@ship.length)).flatten
+      hash = {}
+      letters.each { |letter| hash[letter] = numbers }
+      random_coordinates = numbers.map { |x| "#{letters.shift}#{x}"  }
+        if @computer_board.valid_placement?(@ship, random_coordinates) && @computer_board.ship_does_not_exist_on_cell(random_coordinates)
+          x += 1
+        end
+    end
+    random_coordinates
+  end
+
+  def vertical_placement
+    x = 0
+    until x == 1
+      letters = (65..(65 + (@dimensions - 1))).to_a.map { |ord| ord.chr}
+      numbers = (1..(1 + (@dimensions - 1))).to_a
+      letters = ([letters.each_cons(@ship.length).to_a.sample.sample] * (@ship.length)).flatten
+      numbers = numbers.each_cons(@ship.length).to_a.sample
+      hash = {}
+      letters.each { |letter| hash[letter] = numbers }
+      random_coordinates = numbers.map { |x| "#{letters.shift}#{x}"  }
+        if @computer_board.valid_placement?(@ship, random_coordinates) && @computer_board.ship_does_not_exist_on_cell(random_coordinates)
+          x += 1
+        end
+    end
+    random_coordinates
   end
 
   def place_ships
     x = 0
-    until x == 2
-      ship = @ships[x]
-      #require "pry"; binding.pry
-      horizontal_or_verticle = ["H", "V"].sample
-      if horizontal_or_verticle == "H"
-        letters = (65..(65 + (@dimensions - 1))).to_a.map { |ord| ord.chr}
-        numbers = (1..(1 + (@dimensions - 1))).to_a
-        letters = letters.each_cons(ship.length).to_a.sample
-        numbers = ([numbers.each_cons(ship.length).to_a.sample.sample] * (@dimensions - 1)).flatten
-        hash = {}
-        letters.each { |letter| hash[letter] = numbers }
-        random_coordinates = numbers.map { |x| "#{letters.shift}#{x}"  }
-          if @computer_board.valid_placement?(ship, random_coordinates) && @computer_board.ship_does_not_exist_on_cell(random_coordinates)
-            @computer_board.place(ship, random_coordinates)
-          end
-      else
-        letters = (65..(65 + (@dimensions - 1))).to_a.map { |ord| ord.chr}
-        numbers = (1..(1 + (@dimensions - 1))).to_a
-        letters = ([letters.each_cons(ship.length).to_a.sample.sample] * (@dimensions - 1)).flatten
-        numbers = numbers.each_cons(ship.length).to_a.sample
-        hash = {}
-        letters.each { |letter| hash[letter] = numbers }
-        random_coordinates = numbers.map { |x| "#{letters.shift}#{x}"  }
-          if @computer_board.valid_placement?(ship, random_coordinates) && @computer_board.ship_does_not_exist_on_cell(random_coordinates)
-            @computer_board.place(ship, random_coordinates)
-          end
-      end
-      if @computer_board.valid_placement?(ship, random_coordinates) && @computer_board.ship_does_not_exist_on_cell(random_coordinates)
-        x += 1
-      end
+    2.times do
+      @ship = @ships[x]
+      random_coordinates = [vertical_placement, horizontal_placement].sample
+      @computer_board.place(@ship, random_coordinates)
+      x += 1
     end
   end
+
 end
